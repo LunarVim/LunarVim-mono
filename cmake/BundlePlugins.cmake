6d5945f611f179f8f3b1c61e558a1f76acc93bfb
LostNeophyte <lostneophyte@tuta.io>
2022-12-19 08:48:59 +0100

    test

--- cmake/BundlePlugins.cmake
+++ cmake/BundlePlugins.cmake
@@ -13,6 +13,8 @@ file(REMOVE_RECURSE ${XDG_ROOT})
 
 set(INIT_LUA_PATH ${lvimRepo_SOURCE_DIR}/init.lua)
 
+file(WRITE "$ENV{LUNARVIM_CONFIG_DIR}/config.lua" "${LVIM_VERSION}")
+
 if(WIN32)
   string(REPLACE "/" "\\" INIT_LUA_PATH "${INIT_LUA_PATH}")
   string(REPLACE "/" "\\" ENV{XDG_DATA_HOME} "$ENV{XDG_DATA_HOME}")
@@ -28,28 +30,16 @@ endif()
 message("downloading plugins...")
 message("nvim -u ${INIT_LUA_PATH} --headless -c autocmd User PackerComplete quitall -c PackerInstall")
 execute_process( 
-  COMMAND "nvim" "-u" "${INIT_LUA_PATH}" "--headless" "-c" "autocmd User PackerComplete quitall" "-c" "PackerInstall"
-  TIMEOUT 200
+  COMMAND "nvim" "-u" "${INIT_LUA_PATH}" "--headless" "-c" "quitall"
+  TIMEOUT 20
   RESULT_VARIABLE exit_code
   OUTPUT_VARIABLE output
   ERROR_VARIABLE stderr
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-if(NOT exit_code EQUAL 0 )
-  message(FATAL_ERROR "nvim output: ${exit_code} ${output} ${stderr}")
-else()
-  message("nvim output: ${exit_code} ${output} ${stderr}")
-  message("download complete")
-  install(DIRECTORY 
-    "$ENV{LUNARVIM_RUNTIME_DIR}/site/pack/packer/"
-    DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/lunarvim/plugins/)
-endif()
-
-
-message("nvim -u ${INIT_LUA_PATH} --headless -c autocmd User PackerComplete quitall -c PackerInstall")
 execute_process( 
-  COMMAND "nvim" "-u" "${INIT_LUA_PATH}" "--headless" "-c" "autocmd User PackerComplete quitall" "-c" "lua print(os.getenv(\"LUNARVIM_BASE_DIR\"))"  "-c" "PackerInstall"
-  TIMEOUT 50
+  COMMAND "nvim" "-u" "${INIT_LUA_PATH}" "--headless" "-c" "autocmd User PackerComplete quitall" "-c" "lua print(os.getenv(\"LUNARVIM_BASE_DIR\"))" "-c" "PackerInstall"
+  TIMEOUT 200
   RESULT_VARIABLE exit_code
   OUTPUT_VARIABLE output
   ERROR_VARIABLE stderr
