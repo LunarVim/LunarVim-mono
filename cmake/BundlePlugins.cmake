cd3c3ea7761c9ef6e88cfecfa0f509d9cd425d4b
LostNeophyte <lostneophyte@tuta.io>
2022-12-19 08:11:18 +0100

    try fix

--- cmake/BundlePlugins.cmake
+++ cmake/BundlePlugins.cmake
@@ -11,20 +11,25 @@ set(ENV{LUNARVIM_BASE_DIR} ${lvimRepo_SOURCE_DIR})
 
 file(REMOVE_RECURSE ${XDG_ROOT})
 
+set(INIT_LUA_PATH ${lvimRepo_SOURCE_DIR}/init.lua)
+
 if(WIN32)
-  set(P_SEP "\\")
-else()
-  set(P_SEP "/")
+  string(REPLACE "/" "\\" INIT_LUA_PATH "${INIT_LUA_PATH}")
+  string(REPLACE "/" "\\" ENV{XDG_DATA_HOME} "$ENV{XDG_DATA_HOME}")
+  string(REPLACE "/" "\\" ENV{XDG_CONFIG_HOME} "$ENV{XDG_CONFIG_HOME}")
+  string(REPLACE "/" "\\" ENV{XDG_CACHE_HOME} "$ENV{XDG_CACHE_HOME}")
+  string(REPLACE "/" "\\" ENV{LUNARVIM_RUNTIME_DIR} "$ENV{LUNARVIM_RUNTIME_DIR}")
+  string(REPLACE "/" "\\" ENV{LUNARVIM_CONFIG_DIR} "$ENV{LUNARVIM_CONFIG_DIR}")
+  string(REPLACE "/" "\\" ENV{LUNARVIM_CACHE_DIR} "$ENV{LUNARVIM_CACHE_DIR}")
+  string(REPLACE "/" "\\" ENV{LUNARVIM_BASE_DIR} "$ENV{LUNARVIM_BASE_DIR}")
 endif()
 
-set(INIT_LUA_PATH ${lvimRepo_SOURCE_DIR}/init.lua)
-string(REPLACE "/" "${P_SEP}" INIT_LUA_PATH "${INIT_LUA_PATH}")
 
 message("downloading plugins...")
 message("nvim -u ${INIT_LUA_PATH} --headless -c autocmd User PackerComplete quitall -c PackerInstall")
 execute_process( 
   COMMAND "nvim" "-u" "${INIT_LUA_PATH}" "--headless" "-c" "autocmd User PackerComplete quitall" "-c" "PackerInstall"
-  TIMEOUT 300
+  TIMEOUT 50
   RESULT_VARIABLE exit_code
   OUTPUT_VARIABLE output
   ERROR_VARIABLE stderr
